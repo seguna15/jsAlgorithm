6ab3be2af84d892796ab209f49415671a32a8aa7
seguna15 <seguna15@gmail.com>
2023-02-06 21:08:43 +0100

    objects in JS

--- object.js
+++ object.js
@@ -28,7 +28,7 @@ console.log(user.name);
 //showing how primitive types have object wrappers.
 var num1 = Number(7);
 
-console.log(num1);
+console.log(num1); 
 
 //Mutability of a value is its ability to change or tendency to change.
 //Object are mutable and they are manipulated by reference rather than by values. Objects are sometimes referred to as reference types, this is owing to the fact that they do not store the actual values but are references to its location. 
@@ -44,9 +44,89 @@ console.log(`b: ${b}`);
 //Let us see the mutability of objects;
 
 let x = {a: 2, b: 3, c: 4};
-let y = x;
+let y = x; //referential equality 
 console.log(`x.a: ${x.a}`);
 console.log(`y.a: ${y.a}`);
 x.a = 20;
 console.log(`x.a: ${x.a}`);
 console.log(`y.a: ${y.a}`);
+
+/* 
+** Object Operations include in the following;
+1. Creating Objects.
+2. Set Values to properties and query the values.
+3. Deleting properties
+4. Test properties 
+5. Enumerating Object properties.
+*/
+
+//PROPERTIES ARE KEY (NAME) AND VALUE PAIR. An object cannot have two properties of the same name.
+let myObj = {a: 2, a: 4}; // this is wrong as the last declared property a  will override the initial property a
+console.log(myObj);
+//Object values can be JS values or Getter and setter function
+
+let student = {
+    name: null,
+    age: null,
+    setStudent: function(name, age){
+        this.name = name;
+        this.age = age;
+    },
+    getStudent: function(){
+        return `I am ${this.name} and I am ${this.age} years old.`;
+    }
+};
+
+student.setStudent('Ajennay', 13);
+console.log(student.getStudent());
+
+//Characteristics of JS Object properties
+/* 
+1) Writable: this determines whether the value can be set.
+2) Enumerable: whether a property will be returned when using for/in loop.
+3) Configurable: can it be deleted or altered.
+All built-in object properties are read-only, non-enumerable or non-configurable. However, custom-object or object you create are writeable, enumerable and configurable.
+*/
+
+
+//Creating JS Objects
+//Object literals
+let objUser0 = {};
+//creating objects through object constructor
+let objUser1 = new Object();
+//using Object.create()
+let objUser2 = Object.create({name: 'ajennay', age: 13, school: "Marling Grammar School"});
+//effectively name, age and school prototypes of objUser2 
+let objUser3 = Object.create(Object.prototype); // {} or new Object()
+
+//QUERYING AND SETTING PROPERTIES
+//querying has to do with accessing JS Object Properties
+//Setting has to do with creating properties and their values
+
+const footwear = {
+    manufacturer: "yeezy",
+    color: "red",
+    size: [42, 43, 44],
+    type: "foam-runners",
+    "parent company": 'Adidas',
+};
+
+let sneakerName = footwear.manufacturer;
+let userSize = footwear.size[1];
+let companyName = footwear["parent company"];
+
+console.log(`I own an ${companyName} ${sneakerName} size ${userSize}`);
+
+footwear["country of origin"] = "Philippines";
+
+//An object in JS has what we call "own properties" and also inherit properties of their parent object (prototype object)
+
+const car = { name: "Toyota", country: "Japan"};
+const camry = Object.create(car);
+camry.model = 2014;
+camry.color = "Carbon Black";
+camry.transmission = "Automatic";
+
+console.log(camry.name); //inherited;
+console.log(camry.model); // own property
+console.log(camry.sits);
