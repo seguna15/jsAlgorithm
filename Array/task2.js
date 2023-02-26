5c90969be06fd2f28a57426d7b617b6c453c6909
seguna15 <seguna15@gmail.com>
2023-02-26 12:16:38 +0100

    Task 2 commit

--- Array/task2.js
+++ Array/task2.js
@@ -1,1 +1,9 @@
 //Write an array algorithm that reverse string with odd length in an array e.g ['Hello', 'world', 'We', 'Are', 'Here ] to ['olleH', 'dlrow', 'We', 'era' , 'Here'] 
+
+function reverseOddWords(str) { 
+    return str.split(" ")
+            .map( word => word.length%2 ? [...word].reverse().join("") : word )
+            .join(" ");
+}
+
+console.log(reverseOddWords('Hello World I am here'));
